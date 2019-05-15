c31a5dc730aa1d287f77fbc403eaa8782c5f4ea4
Adam-Czerwinski <adam.czerwinski097@gmail.com>
2019-05-15 21:46:54 +0200

    Utworzono branch 'Develop'
    - Dodano opis plikow xlsx z danymi "legendaXLSX.txt"
    - Dodano model bazy danych "SSIDatabase.architect" (SQL Power Architect)
    - Dodano opis bazy danych "SSIDatabaseDescription.html"
    - Dodano schemat bazy danych "SSIDatabaseSchema.pdf"
    - Dodano plik png obrazujący kierunki wiatru "WindDirections.png"
    
    Dodano do katalogu DataSources > Scripts:
    - Dodano skrypt importujący baze danych "load_database.bat"
    - Dodano narzędzie importujące baze danych "mysql.exe"
    - Dodano skrypt tworzący pustą bazę danych "ssi_database_create.sql"

---
+++ DatabaseSources/Scripts/load_database.bat
@@ -0,0 +1,4 @@
+@echo off
+mysql -u root < ssi_database_create.sql
+echo Done
+pause
---
+++ DatabaseSources/Scripts/ssi_database_create.sql
@@ -0,0 +1,82 @@
+drop database if exists forecast_weather;
+create database forecast_weather;
+use forecast_weather;
+
+create table cities
+(
+    id_cities int unsigned not null auto_increment,
+		primary key(id_cities),
+    name char(42) not null,
+	region enum
+		(
+		"C","N", "E", "S", "W"
+		) not null,
+	is_station boolean not null
+)
+    engine=innodb
+    default character set utf8 collate utf8_unicode_ci;
+	
+	
+create table weather_data
+(
+    id_weather_data int unsigned not null auto_increment,
+		primary key(id_weather_data),
+    city int unsigned not null,
+		foreign key(city) references cities(id_cities),
+	date_ date not null,
+	hour_ time not null,
+    temperature double not null,
+	humidity int not null,
+	wind_direction enum
+		(
+		"C", 
+		"N", "NNE", "NE", "ENE", 
+		"E", "ESE", "SE", "SSE",
+		"S", "SSW", "SW", "WSW",
+		"W", "WNW", "NW", "NNW"
+		) not null,
+	wind_speed decimal not null,
+	cludy decimal not null,
+	visibility decimal not null,
+	data_type enum
+		(
+		"Learning_data", "Testing_data", "User_input_data", "Recursive_data"
+		) not null
+)
+    engine=innodb
+    default character set utf8 collate utf8_unicode_ci;
+	
+create table generations
+(
+    id_generations int unsigned not null auto_increment,
+		primary key(id_generations),
+    neurons_in int not null,
+	neurons_hidden int not null,
+	neurons_out int not null
+)
+    engine=innodb
+    default character set utf8 collate utf8_unicode_ci;
+	
+create table weight
+(
+    id_weight int unsigned not null auto_increment,
+		primary key(id_weight),
+    weight_value text not null
+)
+    engine=innodb
+    default character set utf8 collate utf8_unicode_ci;
+	
+create table learning_process
+(
+    id_learning_process int unsigned not null auto_increment,
+		primary key(id_learning_process),
+	generation int unsigned not null,
+		foreign key(generation) references generations(id_generations),
+	weight int unsigned not null,
+		foreign key(weight) references weight(id_weight),
+	learning_time time not null,
+	total_error double not null,
+	is_learned boolean not null
+)
+    engine=innodb
+    default character set utf8 collate utf8_unicode_ci;
